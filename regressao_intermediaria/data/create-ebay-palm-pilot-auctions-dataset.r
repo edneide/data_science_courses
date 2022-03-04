56f2f760756704b299c1702f1a2cd413eeb96c7e
Edneide Ramalho <edneide.ramalho@gmail.com>
2022-03-04 20:55:21 -0300

    adding files

---
+++ regressao_intermediaria/data/create-ebay-palm-pilot-auctions-dataset.r
@@ -0,0 +1,14 @@
+# Downloaded from https://www.kaggle.com/onlineauctions/online-auctions-dataset
+library(readr)
+library(fst)
+
+# auctions_raw <- read_csv("~/Downloads/361_760_bundle_archive/auction.csv")
+auctions_raw <- read_csv("data/ebay/auction.csv")
+
+
+# Palm pilot has most data, no outliers
+auctions <- auctions_raw %>%
+  filter(item == "Palm Pilot M515 PDA") %>%
+  select(price, openbid, auction_type)
+
+write_fst(auctions, "auctions.fst")
